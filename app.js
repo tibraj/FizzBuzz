40a5c1e3fcc354e8633e4f2da076c0be085e5887
tibraj <tibraj94@gmail.com>
2020-12-28 13:01:39 -0500

    fizzbuzz

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 document.getElementById('submit').addEventListener('submit', fizzBuzz);
+
+function fizzBuzz(){
+    for(let i = 1; n <= 100; n++) {
+        let output = "";
+        if (i % 3 == 0) output += "Fizz";
+        if (i % 5 == 0) output += "Buzz";
+        console.log(output || i);
+    }
+}
